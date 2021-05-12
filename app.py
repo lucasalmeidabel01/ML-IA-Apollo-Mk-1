4a1008c066ec0fa49b12675db2adb7fe201eca7d
lucasalmeidabel01 <lucas.alm.2605@gmail.com>
2021-05-12 15:47:13 -0300

    first time importation 12-05-21

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask,render_template,request
+from flask_bootstrap import Bootstrap
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import joblib
+
+
+
+app = Flask(__name__)
+
+model1 = joblib.load("model/model.pkl")
+
+
+@app.route("/")
+def index():
+	return render_template('index.html')
+
+@app.route("/premierleague")
+def brasileirao():
+	return render_template("premierleague.html")
+
+@app.route("/submit",methods=["GET","POST"])
+def prediction():
+	features = [float(x) for x in request.form.values()]
+	features = np.array(features)
+	scaler = StandardScaler()
+	scaled_features = scaler.fit_transform(features.reshape(-1,1))
+	scaled_features = scaled_features.reshape(1,-1)
+	prediction1 = model1.predict(scaled_features)
+	prediction1 = prediction1[0]
+	return render_template('prediction.html', prediction = prediction1)
+
+if __name__ == 'main':
+	app.run(debug=True)
+bootstrap=Bootstrap(app)
